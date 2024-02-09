176677071ffc26cd7775e3a4ae18e80903370eb5
arkadeep16 <75775078+arkadeep16@users.noreply.github.com>
2024-02-10 01:14:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const status= document.getElementById("status");
+const enterBtn = document.getElementById("enterBtn");
+
+
+function onclick(event) {
+	// console.log("click");
+	document.getElementById("status").innerHTML = "<h1>Entered Metaverse</h1>";
+}
+enterBtn.addEventListener('click', onclick);
